a59c511b4dab84499b4d775defb4aadfb0b2354e
Ivan Bogdan <itsa.designing@gmail.com>
2019-02-22 07:32:42 +0100

    Connected to socket.io.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,50 @@
 </template>
 
 <script>
+import io from 'socket.io-client';
+import axios from 'axios';
+
+const configUrl = 'https://gcm-fra-staging-1.7platform.com:8008/get-lb';
+const query = 'token="token"&clientType="user"';
+const channel = '1d0d6713-b7c9-4f07-ab23-3451a06e8989';
+
+axios.get(configUrl).then((res) => {
+    const socketUrl = res.data.url;
+    const socket = io(socketUrl, {query});
+    socket.on('connect', () => {
+        socket.emit('subscribe', {
+            channel,
+            subChannes: {
+                language: 'en',
+                deliveryPlatform: 'Web',
+                playerUuid: null
+            }
+        })
+    });
+socket.on(channel, (res) => {
+    if(res) {
+        const eventType = res.type;
+        const data = res.data;
+        switch(eventType) {
+        case 'state':
+            console.log('state: ', data);
+            break;
+        case 'new':
+            console.log('new: ', data);
+           break;
+        case 'ball':
+            console.log('ball: ', data);
+        case 'results':
+            console.log('results: ', data);
+            break;
+        case 'countdown':
+            console.log('countdown: ', data);
+            break;
+        }
+    }
+});
+});
+
 export default {
     created() {
         
