265d536521d423b21fd0937ce6e5558cc6494b27
Ivan Bogdan <itsa.designing@gmail.com>
2019-02-23 21:31:44 +0100

    Finished most of the work, now I just need to clean this up. Nasty business... What I've done: Connected with socket.io to gather emitted data.Then, I had to understand what that data is and what to do with it. After I understood it, I started using the data, and combining it with my CSS approach. Developed some logic, which now I see can be done a lot better, or neater. So in my next commit, I'm gonna insert Vuex logic, gonna dissect and clean the App.vue file to smaller pieces, and improve javascript code. So, until next time Git.

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,244 @@
 <template>
-  
+    <div class="container">
+        <div class="wrapper">
+            <div class="countdown">{{ countdownText }}</div> 
+            <div class="balls-grid">
+                <ball-item 
+                    v-for="(ball, index) in balls" 
+                    :ball="ball"
+                    :index="index"
+                    :key="index"
+                />
+                <div v-for="(color, index) in colors" class="balls-grid__color" :class="color" :style="{backgroundColor: color}" :key="index + '_color'"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import io from 'socket.io-client';
-import axios from 'axios';
+    import io from 'socket.io-client';
+    import axios from 'axios';
 
-const configUrl = 'https://gcm-fra-staging-1.7platform.com:8008/get-lb';
-const query = 'token="token"&clientType="user"';
-const channel = '1d0d6713-b7c9-4f07-ab23-3451a06e8989';
-
-axios.get(configUrl).then((res) => {
-    const socketUrl = res.data.url;
-    const socket = io(socketUrl, {query});
-    socket.on('connect', () => {
-        socket.emit('subscribe', {
-            channel,
-            subChannes: {
-                language: 'en',
-                deliveryPlatform: 'Web',
-                playerUuid: null
+    import BallItem from './BallItem.vue';
+    
+    const configUrl = 'https://gcm-fra-staging-1.7platform.com:8008/get-lb';
+    const query = 'token="token"&clientType="user"';
+    const channel = '1d0d6713-b7c9-4f07-ab23-3451a06e8989';
+    
+    export default {
+        components: {
+            BallItem
+        },
+        data: function() {
+            return {
+                colors: ['red', 'green', 'blue', 'purple', 'brown', 'yellow', 'orange', 'white'],
+                balls: Array(48).fill({
+                    id: '',
+                    ball: '',
+                    eventId: '',
+                    active: false,
+                    color: ''
+                }),
+                state: '',
+                countdownTime: false,
+                time: 146
             }
-        })
-    });
-socket.on(channel, (res) => {
-    if(res) {
-        const eventType = res.type;
-        const data = res.data;
-        switch(eventType) {
-        case 'state':
-            console.log('state: ', data);
-            break;
-        case 'new':
-            console.log('new: ', data);
-           break;
-        case 'ball':
-            console.log('ball: ', data);
-        case 'results':
-            console.log('results: ', data);
-            break;
-        case 'countdown':
-            console.log('countdown: ', data);
-            break;
+        },
+        computed: {
+            countdownText() {
+                switch(this.state) {
+                    case 'false':
+                    return 'GAME IN PROGRESS...'
+                    break;
+                    case 'new':
+                    return 'DRAWING BALLS...'
+                    break;
+                    case 'true':
+                    return 'COUNTDOWN: ' + this.time;
+                    break;
+                    default: 
+                    return 'WAIT UNTIL NEXT ROUND...';
+                }
+            }
+        },
+        methods: {
+            activeBall(data) {
+                let newBall = {
+                    id: data.id,
+                    ball: data.ball,
+                    eventId: data.eventId,
+                    color: '',
+                    active: true
+                }
+
+                let modulo = (data.ball%8);
+                
+                switch(modulo) {
+                    case 1:
+                    newBall.color = 'red';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 2:
+                    newBall.color = 'green';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 3:
+                    newBall.color = 'blue';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 4:
+                    newBall.color = 'purple';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 5:
+                    newBall.color = 'brown';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 6:
+                    newBall.color = 'yellow';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 7:
+                    newBall.color = 'orange';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                    case 0:
+                    newBall.color = 'white';
+                    this.balls.splice(data.ball-1, 1, newBall);
+                    break;
+                }
+            }
+        },
+        watch: {
+            countdownTime() {
+                console.log('COUNTDOWN INITIATED');
+                let interval = setInterval(() => {
+                    this.time--;
+                    console.log('TIME:', this.time);
+                    if (this.time == 0) {
+                        clearInterval(interval);
+                    }
+                }, 1000);
+            }
+        },
+        created() {
+            axios.get(configUrl).then((res) => {
+                const socketUrl = res.data.url;
+                const socket = io(socketUrl, {
+                    query
+                });
+                socket.on('connect', () => {
+                    socket.emit('subscribe', {
+                        channel,
+                        subChannes: {
+                            language: 'en',
+                            deliveryPlatform: 'Web',
+                            playerUuid: null
+                        }
+                    })
+                });
+                socket.on(channel, (res) => {
+                    if (res) {
+                        const eventType = res.type;
+                        const data = res.data;
+                        switch (eventType) {
+                            case 'state':
+                                console.log('state: ', data);
+                                switch (data.type) {
+                                    case 'countdown':
+                                    this.state = 'true';
+                                    console.log((Date.now() - data.time));
+                                    this.time = Math.abs(Math.trunc((Date.now() - data.time)/1000) - 146);
+                                    this.countdownTime = !this.countdownTime;
+                                    break;
+                                    case 'ball':
+                                    data.balls.map(ball => this.activeBall(ball));
+                                    break;
+                                }
+                                break;
+                            case 'new':
+                                console.log('new: ', data);
+                                this.state = 'new';
+                                this.balls = Array(48).fill({
+                                    id: '',
+                                    ball: '',
+                                    eventId: '',
+                                    active: false,
+                                    color: ''
+                                });
+                                break;
+                            case 'ball':
+                                console.log('ball: ', data);
+                                this.state = 'false';
+                                this.activeBall(data);
+                            case 'results':
+                                console.log('results: ', data);
+                                break;
+                            case 'countdown':
+                                console.log('countdown: ', data);
+                                this.state = 'true';
+                                this.time = 146;
+                                this.countdownTime = !this.countdownTime;
+                                break;
+                        }
+                    }
+                });
+            });
         }
     }
-});
-});
-
-export default {
-    created() {
-        
-    }
-}
 </script>
 
 <style lang="scss">
+
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: 'Montserrat', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    .container {
+        background-color: #111;
+        min-height: 100vh;
+    }
+
+    .wrapper {
+        margin: auto;
+        width: 80%;
+        padding: 20px;
+        background-color: #333;
+    }
+
+    .main {
+
+    }
+
+    .countdown {
+        background-color: #333;
+        text-align: center;
+        padding-bottom: 1em;
+        color: white;
+    }
+
+    .balls-grid {
+        display: grid;
+        grid-template-columns: repeat(8, 1fr);
+        grid-gap: 20px;
+        background-color: #333;
+        color: gray;
+
+    }
+
+    .balls-grid__color {
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        margin: auto;
+    }
+
+    .balls-grid__color--full {
+
+    }
+
+    
 
 </style>
---
+++ src/BallItem.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <div class="balls-grid__ball" :class="ball.color">
+            <span>{{ this.index + 1 }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        ball: Object,
+        index: Number
+    }
+}
+</script>
+
+<style lang="scss">
+
+    .balls-grid__ball {
+        display: block;
+        width: 30px;
+        height: 30px;
+        border: 1px solid gray;
+        border-radius: 50%;
+        position: relative;
+        font-size: 12px;
+        line-height: 30px;
+        text-align: center;
+        margin: auto;
+    }
+
+    .balls-grid__ball span {
+    }
+
+    .red {
+        border: 1px solid red;
+        background-color: rgba(255, 0, 0, 0.05);
+        color: red;
+    }
+
+    .green {
+        border: 1px solid green;
+        background-color: rgba(0, 128, 0, 0.05);
+        color: green;
+    }
+    
+    .blue {
+        border: 1px solid blue;
+        background-color: rgba(0, 0, 200, 0.05);
+        color: blue;
+    }
+
+    .purple {
+        border: 1px solid blueviolet;
+        background-color: rgba(138, 43, 226, 0.05);
+        color: blueviolet;
+    }
+
+    .brown {
+        border: 1px solid brown;
+        background-color: rgba(165, 42, 42, 0.05);
+        color: brown;
+    }
+
+    .yellow {
+        border: 1px solid yellow;
+        background-color: rgba(255, 255, 0, 0.05);
+        color: yellow;
+    }
+
+    .orange {
+        border: 1px solid orange;
+        background-color: rgba(255, 165, 0, 0.05);
+        color: orange;
+    }
+
+    .white {
+        border: 1px solid white;
+        background-color: rgba(255, 255, 255, 0.05);
+        color: white;
+    }
+
+</style>
